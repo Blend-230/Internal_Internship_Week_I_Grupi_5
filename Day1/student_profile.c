4cb83b90318133d31d65c81d434001689f2ace8a
Blend-230 <br230@bgt.school>
2026-04-14 13:43:53 +0200

    Update student_profile.c

--- Day1/student_profile.c
+++ Day1/student_profile.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name: Emir\n");
-    printf("Surname: Helshani\n");
-    printf("Group: 5\n");
+    printf("Name: Blend\n");
+    printf("Surname: Rama\n");
+    printf("Group number: 5\n");
     return 0;
 }
